ffdfefa11311ead8bf89d5ded06250d60f79ce61
Reetesh <reeteshy15@gmail.com>
2021-08-01 12:18:14 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+
+$domain = "http://" . $_SERVER['SERVER_NAME']."/admin/";
+$apiurl = $domain."web-api/v1/index.php/";
+
+$vendorRedirect = $domain."sales-executive/";
+$vendorimages = $domain."assets/others/images/";
+$vendorScript = $domain."script/vendors/";
+$vendorAssets = $domain."assets/others/";
+$vendoruploades = $domain."assets/images/";
+$userFileaccess =  $domain."userViewFiles";
+ 
+$superAdminRedirect = $domain."admin/";
+$superAdminScript = $domain."script/admin/";
+
+
+ 
+/* ---- User Url Start ---- */
+
+/* ---- User Url ---- */
+$userRedirect = $domain;
+
+/* ---- User Assets ---- */
+$userAssets = $domain."assets/user-assets/";
+
+
+
+// $hotelRedirect = $domain."hotel-admin/";
+$adminRedirect = $domain."admin/";
+
+/* ---- user Script ---- */
+$userScript = $domain."script/user/";
+
+/* ---- User image ---- */
+$profileimages = $domain."assets/profileimg/";
+$api_key = "8b1a9953c4611296a827abf8c47804d7";
+
+/* ---- Super Admin Url Start ---- */
+
+/* ---- Hotel Admin Url ---- */
+
+
+// /* ---- Hotel Admin Assets ---- */
+// $hotelAssets = $domain."assets/hotel/";
+
+// /* ---- Hotel image ---- */
+$newsimage = $domain."assets/images/";
+
+/* ---- Hotel Script ---- */
+
+$editorlink = $domain."assets/js/"
+
+/* ---- Super Admin Url End ---- */
+
+?>
